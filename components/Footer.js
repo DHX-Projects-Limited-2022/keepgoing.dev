d8de5225852a772e97139019eeb787d6e8d9ed32
Dave Hill <99537934+Isdavehill@users.noreply.github.com>
2022-11-01 14:15:19 +0000

    Add copyright

--- components/Footer.js
+++ components/Footer.js
@@ -1,15 +1,13 @@
 
-
+const Year = new Date().getFullYear();
 
 
 const Footer = () => {
     return (
        
-
         <footer className="site-footer">
-            
+            <p>Copyright &copy; {Year} <a href="https://dhxprojects.com/" target="blank">DHX Projects Limited</a>.</p>
         </footer>
-       
     );
 }
 
